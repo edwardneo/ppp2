111290d8555ecc746a35786edfb3f8f523804b50
Edward <edwardneo@gmail.com>
2015-08-30 20:30:56 -0700

    Update Greeting for Months

--- Ch03/greeting.cpp
+++ Ch03/greeting.cpp
@@ -1,8 +1,12 @@
 #include "../std_lib_facilities.h"
 
 int main() {
-	cout << "Please enter your first name (followed "<<"by 'enter'):\n";
+	cout << "Please enter your first name, last name, and age (followed by 'enter'):\n";
 	string first_name;
-	cin >> first_name;
-	cout << "Hello, " << first_name << '\n';
+	string second_name;
+	double age;
+	cin >> first_name >> second_name >> age;
+	double months = age*12;
+	
+	cout << "Hello, " << first_name << " " <<second_name << ". You are " << months <<" months old, right?\n";
 }
