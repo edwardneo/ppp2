774fa530313f0cee8d1400c9a9e69ab434c793a7
Edward <edwardneo@gmail.com>
2015-09-06 18:34:27 -0700

    New print statement program and casting types program

---
+++ Ch03/typeCasting.cpp
@@ -0,0 +1,13 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+	double d = 0;
+	cin >> d;
+	int i = d;
+	char c = i;
+	int i2 = c;
+	cout << "d = " << d
+		 << "\ni = " << i
+		 << "\ni2 = " << i2
+		 << "\nchar = " << c << "\n";
+}
